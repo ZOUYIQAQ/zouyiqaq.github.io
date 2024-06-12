0722bd07eaceb3fca95228b36d9ef5e8a74defc8
ZOUYIQAQ <1483979996@qq.com>
2024-06-12 20:36:43 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://ZOUYIQAQ.github.io/",
-  base: "/ex-test/",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
